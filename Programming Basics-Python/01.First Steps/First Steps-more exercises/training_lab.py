00f5dd45d136f43cc265ec127bcdce11b7646f7a
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2022-03-28 19:31:46 +0300

    Add files via upload

---
+++ Programming Basics-Python/01.First Steps/First Steps-more exercises/training_lab.py
@@ -0,0 +1,5 @@
+width = float(input())
+height = float(input())
+corridor = 100
+hall_wide = width - corridor
+
---
+++ Programming Basics-Python/01.First Steps/First Steps-more exercises/trapezoid_area.py
@@ -0,0 +1,10 @@
+b1 = float(input())
+b2 = float(input())
+h = float(input())
+sum = (b1 + b2) * h / 2
+format_sum = "{:.2f}".format(sum)
+print(format_sum)
+
+
+
+
