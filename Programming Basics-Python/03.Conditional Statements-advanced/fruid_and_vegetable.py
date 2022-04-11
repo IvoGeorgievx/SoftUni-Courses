48646acb3348c85778110e0887f825fd6a81b21f
IvoGeorgievx <ivaylo.georgievxx@gmail.com>
2022-04-11 20:08:38 +0300

    Conditional Statements-advanced

---
+++ Programming Basics-Python/03.Conditional Statements-advanced/fruid_and_vegetable.py
@@ -0,0 +1,31 @@
+product = input()
+type = ''
+
+if product == 'banana':
+    type = 'fruit'
+elif product == "apple":
+    type = 'fruit'
+elif product == "kiwi":
+    type = 'fruit'
+elif product == "cherry":
+    type = 'fruit'
+elif product == "lemon":
+    type = 'fruit'
+elif product == "grapes":
+    type = 'fruit'
+elif product == 'tomato':
+    type = 'vegetable'
+elif product == 'cucumber':
+    type = 'vegetable'
+elif product == 'pepper':
+    type = 'vegetable'
+elif product == 'carrot':
+    type = 'vegetable'
+else:
+    type = 'unknown'
+print(type)
+
+
+
+
+
