bfd2782944654b63491767be71be8874c839332a
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2022-06-03 20:22:27 +0300

    Add files via upload

---
+++ Programming Fundamentals/Functions/Functions-lab/smalles_of_three_numbers.py
@@ -0,0 +1,9 @@
+def smallest_num(a, b, c):
+    print(min(a, b, c))
+
+
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+smallest_num(first_num, second_num, third_num)
+
