6559d74eb0afaa58d7a5d827481ae73ba144b07f
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2022-07-28 18:11:53 +0300

    Add files via upload

---
+++ Programming Fundamentals/RegEx/more exercises/test.py
@@ -0,0 +1,17 @@
+import re
+
+ini_string = input()
+
+
+
+# res1 = "".join(re.findall("[a-zA-Z]+", ini_string))
+# res2 = "".join(re.findall("[0-9]+", ini_string))
+# print(res1)
+# print(res2)
+# res2 = list(map(int, res2))
+# print(sum(res2))
+
+
+# participant = "".join(re.findall("[a-zA-Z]+", participants[0]))
+# participant_points = "".join(re.findall("[0-9]+", participants[0]))
+#     print(participant_points)
