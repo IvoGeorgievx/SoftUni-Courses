a234b7673a259033ab10f1bd43cfdf0e1eacf5d9
IvoGeorgievX <ivaylo.georgievxx@gmail.com>
2022-12-02 09:37:37 +0200

    Inheritance

---
+++ Python OOP/Inheritance/project_multilevel_inheritance/car.py
@@ -0,0 +1,7 @@
+from project_multilevel_inheritence.vehicle import Vehicle
+
+
+class Car(Vehicle):
+
+    def drive(self):
+        return 'driving...'
--- Python OOP/Inheritance/project_multilevel_inheritance/teacher.py
+++
@@ -1,8 +0,0 @@
-from project_multiple_inheritence.employee import Employee
-from project_multiple_inheritence.person import Person
-
-
-class Teacher(Person, Employee):
-
-    def teach(self):
-        return 'teaching...'
