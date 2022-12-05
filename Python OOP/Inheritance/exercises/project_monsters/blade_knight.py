3a8f2a3a4487409ae1afb49e69211655971fdb58
IvoGeorgievX <ivaylo.georgievxx@gmail.com>
2022-12-05 09:40:23 +0200

    Inheritance

---
+++ Python OOP/Inheritance/exercises/project_monsters/blade_knight.py
@@ -0,0 +1,8 @@
+from exercise.project_monsters.dark_knight import DarkKnight
+
+
+class BladeKnight(DarkKnight):
+
+    def __init__(self, username, level):
+        super().__init__(username, level)
+
