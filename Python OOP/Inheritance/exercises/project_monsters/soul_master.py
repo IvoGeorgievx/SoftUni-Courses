d2685513d2053e8c2ce91097470602f72ad67f39
IvoGeorgievX <ivaylo.georgievxx@gmail.com>
2022-12-10 19:10:54 +0200

    Inheritance

---
+++ Python OOP/Inheritance/exercises/project_monsters/soul_master.py
@@ -0,0 +1,9 @@
+from exercise.project_monsters.dark_wizard import DarkWizard
+
+
+class SoulMaster(DarkWizard):
+
+    def __init__(self,username, level):
+        super().__init__(username, level)
+
+
