8d72dea8438e5f18acc208e35e768bfd69dfd867
IvoGeorgievX <ivaylo.georgievxx@gmail.com>
2023-02-01 18:23:55 +0200

    Projects

---
+++ Python OOP/Projects/car/car/sports_car.py
@@ -0,0 +1,19 @@
+from project73.car.car import Car
+
+
+class SportsCar(Car):
+    MIN_SPEED = 400
+    MAX_SPEED = 600
+
+    def __init__(self, model, speed_limit):
+        super().__init__(model, speed_limit)
+
+    @property
+    def speed_limit(self):
+        return self.__speed_limit
+
+    @speed_limit.setter
+    def speed_limit(self, value):
+        if value > self.MAX_SPEED or value < self.MIN_SPEED:
+            raise ValueError(f"Invalid speed limit! Must be between {self.MIN_SPEED} and {self.MAX_SPEED}!")
+        self.__speed_limit = value
