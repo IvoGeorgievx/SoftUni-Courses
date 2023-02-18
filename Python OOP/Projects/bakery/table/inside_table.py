3d3c07678e5175359780c324a800c770a8cc8c76
IvoGeorgievX <ivaylo.georgievxx@gmail.com>
2023-02-18 13:16:45 +0200

    Projects

---
+++ Python OOP/Projects/bakery/table/inside_table.py
@@ -0,0 +1,26 @@
+from project.table.table import Table
+
+
+class InsideTable(Table):
+    def __init__(self, table_number: int, capacity: int):
+        super().__init__(table_number, capacity)
+        
+    @property
+    def min_number(self):
+        return 1
+
+    @property
+    def max_number(self):
+        return 50
+
+    @property
+    def table_number(self):
+        return self.__table_number
+
+    @table_number.setter
+    def table_number(self, value):
+        if self.min_number > value or self.max_number < value:
+            raise ValueError("Inside table's number must be between 1 and 50 inclusive!")
+        self.__table_number = value
+
+
