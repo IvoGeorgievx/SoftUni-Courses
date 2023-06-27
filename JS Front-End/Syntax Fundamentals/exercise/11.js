14c1675cb9a93155544c7dd5827677f02ad15c94
IvoGeorgievX <ivaylo.georgievxx@gmail.com>
2023-06-27 14:51:17 +0300

    exercises

--- JS Front-End/Syntax Fundamentals/exercise/11.js
+++ JS Front-End/Syntax Fundamentals/exercise/11.js
@@ -1,10 +1,32 @@
 function roadRadar(currentSpeed, area) {
-    const speeds = {
-        "motorway": 130,
-        "interstater": 90,
-        "city": 50,
-        "residential": 20
-    }
+	const speeds = {
+		motorway: 130,
+		interstate: 90,
+		city: 50,
+		residential: 20,
+	};
 
-    
+	let difference = currentSpeed - speeds[area];
+	let status = "";
+
+	if (difference <= 0) {
+		console.log(`Driving ${currentSpeed} km/h in a ${speeds[area]} zone`);
+	} else if (difference <= 20) {
+		status = "speeding";
+		console.log(
+			`The speed is ${difference} km/h faster than the allowed speed of ${speeds[area]} - ${status}`
+		);
+	} else if (difference <= 40) {
+		status = "excessive speeding";
+		console.log(
+			`The speed is ${difference} km/h faster than the allowed speed of ${speeds[area]} - ${status}`
+		);
+	} else {
+		status = "reckless driving";
+		console.log(
+			`The speed is ${difference} km/h faster than the allowed speed of ${speeds[area]} - ${status}`
+		);
+	}
 }
+ 
+roadRadar(55, "city");
