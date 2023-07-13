6053e35735462d9a049ed3ed5ab5f1ebbd9df32a
IvoGeorgievX <ivaylo.georgievxx@gmail.com>
2023-07-13 10:05:24 +0300

    DOM and Events

---
+++ JS Front-End/DOM and Events/02. Show-More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+	const expand = document.getElementById("text");
+	const hide = document.getElementById("more");
+
+	expand.style.display = "block";
+	hide.style.display = "none";
+}
